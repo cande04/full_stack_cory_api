3b8d51f4a6c15deb3211c2a4b1c21844e8678a85
cande04 <cory.anders.atc@gmail.com>
2019-06-07 10:23:47 -0400

    Rename project
    renamed project for initial set up

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require 'action_controller/railtie'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Full_Stack_cory
+module FullStackCory
   # :nodoc:
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified
